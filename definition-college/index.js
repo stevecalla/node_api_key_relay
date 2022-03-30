b33e12051e23f2525bcbbbbaabf61cb7f95b7a8c
Steve <callasteven@gmail.com>
2022-03-30 13:10:33 -0600

    Adjustment

--- definition-college/index.js
+++ definition-college/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fetch = require("node-fetch");
 
 const fetchDefinition = async (searchtext) => {
-  const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchtext}?key=${process.env.ELEMENTARY_API_KEY}`;
+  const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchtext}?key=${process.env.COLLEGE_API_KEY}`;
   // https://www.dictionaryapi.com/api/v3/references/collegiate/json/${solution}?key=d6ad76fd-5324-4925-834b-17a06efafce6
   try {
     const definitionStream = await fetch(url);
